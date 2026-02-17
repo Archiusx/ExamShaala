6fab66a9783ce534dabf5426d538331b517cd322
Piyush Rajesh Deshkar <shacx8@gmail.com>
2026-02-17 08:25:05 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- firebase.js
+++ firebase.js
@@ -18,13 +18,13 @@ import {
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_4_adS0YQs8bGMbEvNSpLpW3BpCdvIAU",
-  authDomain: "mark1-7ce7e.firebaseapp.com",
-  projectId: "mark1-7ce7e",
-  storageBucket: "mark1-7ce7e.appspot.com",
-  messagingSenderId: "147908886392",
-  appId: "1:147908886392:web:7d209960ba65868172128d",
-  measurementId: "G-6J34J730EW"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
